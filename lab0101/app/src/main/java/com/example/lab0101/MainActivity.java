3846cffd1847423e6bfd3f47118f3fa71cde10f0
Ksenia Kuvshinova <kseniakuv265@gmail.com>
2021-12-28 23:53:05 +0300

    first lab

---
+++ lab0101/app/src/main/java/com/example/lab0101/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.lab0101;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn1;
+    TextView txt1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1=findViewById(R.id.mybtn1);
+        txt1=findViewById(R.id.mytxt1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                txt1.setText("Goodbye");
+            }
+        });
+    }
+}
